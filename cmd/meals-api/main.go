04d1b94af06c0e3ba37da16275953f4e780fd98a
Joseph Cosentino <jcosentino11@gmail.com>
2020-06-18 04:38:08 +0100

    add dummy webserver

--- cmd/meals-api/main.go
+++ cmd/meals-api/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Printf("Hello World")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
